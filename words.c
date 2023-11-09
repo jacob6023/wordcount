b065fd936ca0f8ed242e15933161649124807958
jacob6023 <147463768+jacob6023@users.noreply.github.com>
2023-11-08 22:01:23 -0500

    Update words.c

--- words.c
+++ words.c
@@ -4,8 +4,70 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#define TABLE_SIZE 1000
+#define MAX_WORD_LENGTH 100
 
 
+typedef struct Node {
+    char* word;
+    int count;
+    struct Node* next;
+} Node;
+
+typedef struct HashTable {
+    Node* array[TABLE_SIZE];
+} HashTable;
+
+int hash(char* word) {
+    int hash = 0;
+    for (int i = 0; word[i] != '\0'; i++) {
+        hash = (hash * 31 + word[i]) % TABLE_SIZE;
+    }
+    return hash;
+}
+
+void insert(HashTable* ht, char* word) {
+    int index = hash(word);
+    Node* temp = ht->array[index];
+
+    while (temp != NULL) {
+        if (strcmp(temp->word, word) == 0) {
+            temp->count++;
+            return;
+        }
+        temp = temp->next;
+    }
+
+    Node* newNode = malloc(sizeof(Node));
+    newNode->word = strdup(word);
+    newNode->count = 1;
+    newNode->next = ht->array[index];
+    ht->array[index] = newNode;
+}
+
+void displayWordCounts(HashTable* ht) {
+    printf("Word Counts:\n");
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        Node* temp = ht->array[i];
+        while (temp != NULL) {
+            printf("%s: %d\n", temp->word, temp->count);
+            temp = temp->next;
+        }
+    }
+}
+
+void freeHashTable(HashTable* ht) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        Node* current = ht->array[i];
+        while (current != NULL) {
+            Node* temp = current;
+            current = current->next;
+            free(temp->word);
+            free(temp);
+        }
+    }
+    free(ht);
+}
 
 int main(int argc, char *argv[]){
     
