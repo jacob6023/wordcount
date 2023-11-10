a9e846b0092672c03e7d866b8d344c120d463d17
MuffinCrow <dbotney@yahoo.com>
2023-11-09 20:47:06 -0500

    Moved things around and finished getting words

--- words.c
+++ words.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #define TABLE_SIZE 1000         // Size of the hash table
 #define MAX_WORD_LENGTH 100    // Maximum word length
 
@@ -16,6 +18,131 @@ typedef struct Node {
 typedef struct HashTable {
     Node* array[TABLE_SIZE];  // Array of Nodes (the hash table)
 } HashTable;
+
+int checkAscii(char c) {
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c == '\'')) {
+        return 0;
+    } else if (c == '-') {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+void fileRead(char *path) {
+    char buf[2];
+    char* input = malloc(1);
+    int length = 0;
+    int bytes = 0, fd;
+    int inWord = 0, hyphenCheck = 0;
+
+    fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        printf("Unable to open file at path: %s\n", path);
+        return;
+    }
+    memset(input, 0, 1);
+    while ((bytes = read(fd, buf, 1)) > 0) {
+        //printf("Letter: %c\n", buf[0]);
+        if (bytes == 0 && inWord == 1) {
+            //insert(ht, input);
+            //printf("Word: %s\n\n", input);
+        } else {
+            if (checkAscii(buf[0]) == 1 && hyphenCheck == 0 && inWord == 1) {
+                hyphenCheck = 1;
+            } else {
+                if (checkAscii(buf[0]) == 0) {
+                    inWord = 1;
+                    if (hyphenCheck == 1) {
+                        hyphenCheck = 0;
+                        length += 1;
+                        input = realloc(input, length + 1);
+                        strcat(input, "-");
+                    }
+                    length += 1;
+                    input = realloc(input, length + 1);
+                    strncat(input, &buf[0], 1);
+                } else {
+                    hyphenCheck = 0;
+                    if (inWord == 1) {
+                        //insert(ht, input);
+                        //printf("Word: %s\n\n", input);
+                        length = 0;
+                        free(input);
+                        input = NULL;
+                        input = malloc(1);
+                        inWord = 0;
+                        memset(input, 0, 1);
+                    }
+                }
+            }
+            
+        }
+    }
+    free(input);
+    close(fd);
+}
+
+void recursiveSearch(HashTable* ht, char *path, int passes) {
+    struct dirent *entry;
+    struct stat info;
+
+    DIR *dir = opendir(path);
+
+    if (passes == 0) {
+        stat(path, &info);
+        if (S_ISREG(info.st_mode)) {
+            printf("File from argument: %s\n", path);
+
+            //Insert call to read here
+
+            return;
+        }
+    }
+    
+    if (dir == NULL) {
+        printf("Error opening path %s during recursiveSearch.\n", path);
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        //skips "." and ".." directory entries
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        char entryPath[4096];
+        snprintf(entryPath, sizeof(entryPath), "%s/%s", path, entry->d_name);
+
+        printf("%s\n", entryPath);
+
+        if (stat(entryPath, &info) != 0) {
+            printf("Error getting file info for %s in recursiveSearch.\n", path);
+            return;
+        }
+
+        //checks if it is a directory
+        if(S_ISDIR(info.st_mode)) {
+            //Is a directory
+            printf("Directory\n");
+            recursiveSearch(ht, entryPath, passes + 1);
+        } else if (S_ISREG(info.st_mode)) {
+            printf("File: %s\n", entryPath);
+
+            int nameLength = strlen(entry->d_name);
+            if (nameLength > 4 && strcmp(entry->d_name + nameLength - 4, ".txt") == 0) {
+                printf("File ends in .txt\n\n");
+
+                //Insert call to read here
+
+            }
+            
+        }
+
+        //if not a file or directory, ignores
+    }
+    closedir(dir);
+}
 
 // Function to calculate the hash value for a word
 int hash(char* word) {
@@ -77,67 +204,42 @@ void freeHashTable(HashTable* ht) {
     free(ht);
 }
 
-void recursiveSearch(char *path, int passes) {
-    struct dirent *entry;
-    struct stat info;
-
-    DIR *dir = opendir(path);
-
-    if (passes == 0) {
-        stat(path, &info);
-        if (S_ISREG(info.st_mode)) {
-            printf("File from argument: %s\n", path);
-
-            //Insert call to read here
-
-            return;
-        }
-    }
+/*
+void wordProcessor(HashTable* ht, char* input, int length) {
+    int inWord = 0;
     
-    if (dir == NULL) {
-        printf("Error opening path %s during recursiveSearch.\n", path);
-        return;
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        //skips "." and ".." directory entries
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+    for (int i = 0; i < length; i++) {
+        if (checkAscii(input[i]) > 1 && inWord == 0) {
+            length -= 1;
+            realloc(input + 1, length);
+            i--;
             continue;
-        }
-
-        char entryPath[4096];
-        snprintf(entryPath, sizeof(entryPath), "%s/%s", path, entry->d_name);
-
-        printf("%s\n", entryPath);
-
-        if (stat(entryPath, &info) != 0) {
-            printf("Error getting file info for %s in recursiveSearch.\n", path);
-            return;
-        }
-
-        //checks if it is a directory
-        if(S_ISDIR(info.st_mode)) {
-            //Is a directory
-            printf("Directory\n");
-            recursiveSearch(entryPath, passes + 1);
-        } else if (S_ISREG(info.st_mode)) {
-            printf("File: %s\n", entryPath);
-
-            int nameLength = strlen(entry->d_name);
-            if (nameLength > 4 && strcmp(entry->d_name + nameLength - 4, ".txt") == 0) {
-                printf("File ends in .txt\n\n");
-
-                //Insert call to read here
-
+        } else if (checkAscii(input[i]) == 0) {
+            inWord = 1;
+        } else if (checkAscii(input[i]) == 1 && inWord == 1) {
+            if (checkAscii(input[i+1]) != 0) {
+                char export[i+1];
+                strncpy(export, input, i+1);
+                realloc(input + i + 1, length - i - 1);
+                length = length - i - 1;
+                i = 0;
+                inWord = 0;
+                //Send export to insertWord
             }
-            
+        } else if (checkAscii(input[i]) > 1 && inWord == 1) {
+            char export[i+1];
+            strncpy(export, input, i+1);
+            realloc(input + i + 1, length - i - 1);
+            length = length - i - 1;
+            i = 0;
+            inWord = 0;
+            //Send export to insertWord
         }
-
-        //if not a file or directory, ignores
     }
-    closedir(dir);
 }
+*/
 
 int main(int argc, char *argv[]){
     
 }
+//HashTable* ht, 
